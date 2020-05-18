802d713d182feb65546c6f112e4fee4415886f32
doonnoop <pd1364@nyu.edu>
2020-05-18 11:07:31 -0400

    move list

--- src/components/RetroListItem.js
+++ src/components/RetroListItem.js
@@ -4,6 +4,7 @@ import {
   startDeleteRetro,
   startVoteRetro,
   startEditRetro,
+  startAddRetro,
 } from "../actions/retro";
 import voteImg from "../../public/images/vote.png";
 
@@ -55,6 +56,17 @@ class RetroListItem extends React.Component {
     }
   };
 
+  handleMove = (toList) => {
+    this.props.startDeleteRetro({
+      id: this.props.retro.id,
+      list: this.props.list,
+    });
+    this.props.startAddRetro({
+      content: this.props.retro.content,
+      list: toList,
+    });
+  };
+
   render() {
     return (
       <div className="retro-item">
@@ -73,10 +85,38 @@ class RetroListItem extends React.Component {
         )}
         <span className="retro-item__catalog">{this.props.retro.votes}</span>
         <div>
-          <button onClick={this.handleVote} className="retro-item__vote">
-            <img src={voteImg} />
-          </button>
-          <button onClick={this.handleMove}>MoveToList</button>
+          <div>
+            <button onClick={this.handleVote} className="retro-item__vote">
+              <img src={voteImg} />
+            </button>
+            <button
+              onClick={() => this.handleMove("Went Well")}
+              style={{
+                display: this.props.list == "Went Well" ? "none" : "inline",
+              }}
+              className="move-button"
+            >
+              Went Well
+            </button>
+            <button
+              onClick={() => this.handleMove("To Improve")}
+              style={{
+                display: this.props.list == "To Improve" ? "none" : "inline",
+              }}
+              className="move-button"
+            >
+              To Improve
+            </button>
+            <button
+              onClick={() => this.handleMove("Action Items")}
+              style={{
+                display: this.props.list == "Action Items" ? "none" : "inline",
+              }}
+              className="move-button"
+            >
+              Action Items
+            </button>
+          </div>
           <button className="retro-item__delete" onClick={this.handleDelete}>
             x
           </button>
@@ -91,6 +131,7 @@ const mapDispatchToProps = (dispatch) => {
     startDeleteRetro: (data) => dispatch(startDeleteRetro(data)),
     startVoteRetro: (data) => dispatch(startVoteRetro(data)),
     startEditRetro: (data) => dispatch(startEditRetro(data)),
+    startAddRetro: (data) => dispatch(startAddRetro(data)),
   };
 };
 
